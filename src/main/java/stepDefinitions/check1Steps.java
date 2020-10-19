d82e2e60c2f94bb4d6ac15c49dacd35ec4a6988f
lingam.mahesh753@gmail.com <Myname.babi537>
2020-10-19 14:27:51 +0530

    modified for checks

--- src/main/java/stepDefinitions/check1Steps.java
+++ src/main/java/stepDefinitions/check1Steps.java
@@ -2,31 +2,31 @@ package stepDefinitions;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.BeforeStep;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
+import org.junit.Assert;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 public class check1Steps {
     WebDriver driver;
+
     @Before
-    public void setup(){
-        System.setProperty("webdriver.chrome.driver","C:\\Webdriver\\chromedriver.exe");
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Webdriver\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     }
+
     @Given("the user opens Google Webiste")
     public void the_user_opens_Google_Webiste() {
         driver.get("https://www.google.com/");
@@ -34,7 +34,7 @@ public class check1Steps {
 
     @When("User enters search Keyword")
     public void user_enters_search_Keyword() {
-        WebDriverWait wait= new WebDriverWait(driver,60);
+        WebDriverWait wait = new WebDriverWait(driver, 60);
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//img[contains(@alt,'Google')]"))));
         driver.findElement(By.xpath("//input[@title='Search']")).sendKeys("Hello");
     }
@@ -47,15 +47,81 @@ public class check1Steps {
     }
 
 
-
     @Then("User gets the search results")
     public void user_gets_the_search_results() {
-        WebDriverWait wait= new WebDriverWait(driver,60);
+        WebDriverWait wait = new WebDriverWait(driver, 60);
         wait.until(ExpectedConditions.titleIs("Hello - Google Search"));
     }
 
     @After
-    public void terminate(){
+    public void terminate() {
         driver.quit();
     }
+
+    @When("User enters new search Keyword")
+    public void userEntersNewSearchKeyword() {
+        WebDriverWait wait = new WebDriverWait(driver, 60);
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//img[contains(@alt,'Google')]"))));
+        driver.findElement(By.xpath("//input[@title='Search']")).sendKeys("Hi");
+    }
+
+    @Then("User gets the new search results")
+    public void userGetsTheNewSearchResults() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, 60);
+        wait.until(ExpectedConditions.titleIs("Hi - Google Search"));
+        WebElement element = driver.findElement(By.xpath("//input[@name='q' and @type='text']"));
+        keyStroke(Keys.PAGE_DOWN);
+        element.clear();
+        element.sendKeys("Hello");
+        Thread.sleep(2000);
+        keyStroke(Keys.ENTER);
+        Thread.sleep(9000);
+        keyStroke(Keys.PAGE_DOWN);
+        Actions act = new Actions(driver);
+        act.keyDown(Keys.CONTROL).click(driver.findElement(By.xpath("//span[contains(text(),'Wikipedia')]"))).keyUp(Keys.CONTROL).build().perform();
+        switchToWindow("Hello (Adele song) - Wikipedia");
+        System.out.println("Succesfully switched to the new window");
+        Thread.sleep(9000);
+        keyStroke(Keys.PAGE_DOWN);
+        keyStroke(Keys.PAGE_DOWN);
+        Thread.sleep(20000);
+
+
+
+    }
+
+    public void keyStroke(Keys keyToPress) throws InterruptedException {
+        Actions act = new Actions(driver);
+        act.sendKeys(keyToPress).build().perform();
+        Thread.sleep(3000);
+    }
+
+    public void switchToWindow(String name){
+        try{
+            ArrayList<String> windows = new ArrayList<String>(driver.getWindowHandles());
+            if(windows.size()>0) {
+                System.out.println("windows size: "+windows.size());
+//                for(int i=0;i<windows.size();i++){
+//                    if(windows.get(i)){
+                        driver.switchTo().window(name);
+//                        break;
+//                    }else{
+//                        System.out.println("Not found at i = "+i);
+//                    }
+                }
+//            }else{
+//                Assert.fail("No Window is opened");
+//            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 }
+
+//    public void keyStroke(String keyToPress) throws InterruptedException {
+//    }
+//        Actions act = new Actions(driver);
+//
+//        act.sendKeys(key).build().perform();
+//        Thread.sleep(3000);
+//}
--- src/test/Features/check2.feature
+++ src/test/Features/check2.feature
@@ -2,6 +2,6 @@
 Feature: Pipeline check1
   Scenario: Open Google website
     Given the user opens Google Webiste
-    When User enters search Keyword
+    When User enters new search Keyword
     And user clicks on search button
-    Then User gets the search results
+    Then User gets the new search results
